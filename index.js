fab9d547bd158506dae6114669a3009dbda191f2
lauraszm <lauraszm@gmail.com>
2024-09-08 12:43:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import * as fs from 'node:fs';
+
+function crearDoc(arr, tipoDato) {
+    const tiposDatosValidos = [
+        "boolean",
+        "number",
+        "string"
+    ]
+
+    let comparar = (a, b) => {return a-b};
+
+    let tipoDatoParam = tiposDatosValidos.find(el => el === tipoDato);
+    if(tipoDatoParam == null){
+        throw new Error("El tipo de dato solicitado no es valido");
+    } else {
+        const newArray = arr.filter(el => typeof(el) === tipoDatoParam).sort(comparar);
+        const newFile = fs.writeFileSync("nuevo.txt", newArray.join(","))
+    }
+
+}
+
+const lista = [2,10,"a",4,"b",6,"d",true,"e",9,1,"z",12,"r", "c", false];
+try {
+    crearDoc(lista, "numeros");
+} catch (error) {
+    console.log(error)
+}
