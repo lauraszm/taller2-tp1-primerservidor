4e600d6ce9bd9e01cdabc564fcfba1b816629ac2
lauraszm <lauraszm@gmail.com>
2024-09-08 16:00:41 -0300

    cambio string a variable

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ function crearDoc(arr, tipoDato) {
 }
 
 const lista = [2,10,"a",4,"b",6,"d",true,"e",9,1,"z",12,"r", "c", false];
+let tipo = "number"
 try {
-    crearDoc(lista, "numeros");
+    crearDoc(lista,tipo);
 } catch (error) {
     console.log(error)
 }
